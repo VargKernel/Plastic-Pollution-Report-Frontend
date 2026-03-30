b15708cf35ec15c632f9e717b8afd4795105ac22
VargKernel <sbox.simple@protonmail.com>
2026-03-30 08:18:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/environmental-homework/',
+    base: '/Plastic-Pollution-Report-2026/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
